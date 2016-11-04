884c45778499ffe5c3538d9786d57019010fba2a
sanatpasricha <sanatpasricha@gmail.com>
2016-11-04 12:52:42 +0530

    Add files via upload

---
+++ Jscript1.js
@@ -0,0 +1,10 @@
+﻿
+console.log("javascript practise1")
+var name = "sanat"
+console.log("javascript practise2")
+var age = 33
+console.log("javascript practise3")
+var dateofbirth = "24th July 1983"
+console.log("javascript practise4")
+var placeofbirth = "delhi"
+console.log("javascript practise5")
